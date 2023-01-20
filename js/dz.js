a71ebf77ec4438fda1db1a135963550ffe670cf7
fedorbor <112242236+fedorbor@users.noreply.github.com>
2023-01-20 17:15:13 +0200

    dz

---
+++ js/dz.js
@@ -0,0 +1,50 @@
+// 1
+console.log("1");
+const k1="5px"
+console.log(Number.parseInt(k1));
+const k2="12djd334"
+console.log(Number.parseInt(k2));
+const k3="12.45asdwe12"
+console.log(Number.parseInt(k3));
+const k4="qwqweeewq"
+console.log(Number.parseInt(k4));
+// 2
+console.log("2");
+console.log("1");
+const k5="5px"
+console.log(Number.parseFloat(k5));
+const k6="12djd334"
+console.log(Number.parseFloat(k6));
+const k7="12.45asdwe12"
+console.log(Number.parseFloat(k7));
+const k8="qwqweeewq"
+console.log(Number.parseFloat(k8));
+// 3
+console.log("3");
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+// 4
+console.log("4");
+console.log ( (Math.round(Math.random() *( 19 - 3 + 1) + 3)  )) ;
+// 5
+console.log("5");
+const result = 5+5+'5';
+console.log(result);
+console.log(typeof(result));
+// 6
+console.log("6");
+const email="borisenkofedor12gmail.com"
+console.log(email.includes("@"));
+console.log(email.length)
+// 7    
+console.log("7");
+let fullName = "My " + "name " +"is ";
+fullName = fullName+"Victor"
+console.log(fullName)
+
+// 8    
+console.log("8");
+const userName = "Федір";
+const payment = "400";
+console.log("Дякуємо, "+userName+"! До сплати " +payment + " гривень" )
+// Дякуємо, Федір! До сплати 400 гривень
